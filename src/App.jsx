f489e1d9173789a946096107060888089aaf7bc9
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-03-17 15:31:57 +0530

    create pages and setup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SingUp from './Pages/SingUp'
+import Profile from './Pages/Profile'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SingUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Pages/SingUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingUp = () => {
+    return (
+        <div>
+            <h1>SignUp</h1>
+        </div>
+    )
+}
+
+export default SingUp
